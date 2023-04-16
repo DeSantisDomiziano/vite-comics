e3d8948b27540f43807d3d3be7f8dbf13bd52d7c
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-04-16 15:45:31 +0200

    add footer bottom

--- src/components/footer-components/FooterBottom.vue
+++ src/components/footer-components/FooterBottom.vue
@@ -1,15 +1,43 @@
 <script>
-    export default {
-        
-    }
+import SocialMedia from '../social-media/SocialMedia.vue'
+export default {
+    components: {
+        SocialMedia
+    },
+    name: 'FooterBottom',
+}
 </script>
 
 <template>
-    <div>
+    <footer>
 
-    </div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <button type="button" class="btn text-uppercase fw-semibold">Sign-up now!</button>
+                </div>
+                <div class="col">
+                    <SocialMedia/>
+                </div>
+            </div>
+        </div>
+
+    </footer>
 </template>
 
 <style lang="scss" scoped>
+@use '../../assets/scss/partials/variables' as *;
+
+footer {
+    background-color: $footerBottom;
+    padding: 2rem 0;
+
+    .btn {
+        padding: 0.5rem 0.75rem;
+        border: 3px solid $primary;
+        border-radius: 0;
+        color: $light;
+    }
+}
 
 </style>
---
+++ src/components/social-media/SocialMedia.vue
@@ -0,0 +1,62 @@
+<script>
+    export default {
+        name: 'SocialMedia',
+        data() {
+            return{
+                socials: [
+                    {
+                        src: 'footer-facebook',
+                        href: '#'
+                    },
+                    {
+                        src: 'footer-periscope',
+                        href: '#'
+                    },
+                    {
+                        src: 'footer-pinterest',
+                        href: '#'
+                    },
+                    {
+                        src: 'footer-twitter',
+                        href: '#'
+                    },
+                    {
+                        src: 'footer-youtube',
+                        href: '#'
+                    },
+                ]
+            }
+        },
+        methods: {
+            getImageUrl(name) {
+                return new URL(`../../assets/img/${name}.png`, import.meta.url).href
+            }
+        }      
+    }
+</script>
+
+<template>
+
+    
+        
+        <ul class="d-flex justify-content-end">
+            <li>
+                <h5 class="text-uppercase fw-semibold mt-2 me-4">follow us</h5>
+            </li>
+            <li class="p-1" v-for="social in socials">
+                <img :src="getImageUrl(social.src)" :alt="social.src">
+            </li>
+        </ul>
+    
+    
+    
+</template>
+
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/variables' as *;
+
+h5 {
+    float: left;
+    color: $primary;
+}
+</style>
