b93f3ef917052e1f1a0ea8072df1c023b36429e6
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-04-16 10:39:00 +0200

    add assets and star SiteHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script>
+import SiteHeader from './components/SiteHeader.vue'
+export default {
+    components: {
+        
+    },
+    data(){
+
+    }
+}
 </script>
 
 <template>
+
+    <SiteHeader></SiteHeader>
 
 </template>
 
